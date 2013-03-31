1f988318c67bd29b4be3bf7f8b3a338650813809
Luke Petrolekas <luke.petrolekas@gmail.com>
2013-03-31 11:04:59 -0400

    Refactor objects passed to onclick event handler

--- js/quizzler.js
+++ js/quizzler.js
@@ -12,22 +12,24 @@
 		//---------------------------------------------------------------------
 		
 		onBoardClick = function(event) {
+			var game = event.data.game;
+			var board = event.data.board;
 			var answer;
 			var question;
 
-			answer = ich.answer_text_template({answer : event.data.answer});
-			question = ich.question_text_template({question : event.data.question}).hide()
+			answer = ich.answer_text_template({answer : event.data.jsonData.answer});
+			question = ich.question_text_template({question : event.data.jsonData.question}).hide()
 				
-			$("#game").append(answer);
-			$("#game").append(question);
+			game.append(answer);
+			game.append(question);
 
 			answer.disableSelection();
 			answer.dblclick(function(){question.fadeIn(); answer.remove();});
 
 			question.disableSelection();
-			question.dblclick(function(){$("#board").fadeIn(); question.remove();});
+			question.dblclick(function(){board.fadeIn(); question.remove();});
 			
-			$("#board").fadeOut();
+			board.fadeOut();
 		};
 		
 		//---------------------------------------------------------------------
@@ -35,11 +37,12 @@
 		
 		
 		$.getJSON("quiz/quiz_data.json", function(json) {
+			var game = $("#game");
 			var quizData = json.quiz_data;
 		
 			//prepare board
 			var board = ich.board_template();
-			$("#game").append(board);
+			game.append(board);
 			var round = quizData.rounds[0];
 			var topics = round.topics;
 		
@@ -59,7 +62,7 @@
 			for (var k = 0; k < 5; k++){
 				for (var j = 0; j < topics.length; j++){
 					var x = ich.answer_template({"value" : topics[j].questions[k].value});
-					x.dblclick({answer : topics[j].questions[k].answer, question : topics[j].questions[k].question }, onBoardClick);
+					x.dblclick({jsonData : topics[j].questions[k], board : board, game : game }, onBoardClick);
 					row.append(x);
 				}
 				//add end to last column per row
